f8a5a5699ab733aea54280dfab9a3861c60bc84d
Peter Mikulasko <t00148178@365s.ittralee.ie>
2015-11-24 16:58:58 +0000

    addedd empty classess for navigation

--- PayGateApp.java
+++ PayGateApp.java
@@ -2,9 +2,10 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 
 import java.awt.Container;
-import java.awt.MenuBar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
@@ -14,10 +15,15 @@ import java.awt.FlowLayout;
 
 public class PayGateApp extends JFrame implements ActionListener {
 	
-	JMenuBar jMenuBar;
-	JMenu fileMenu,accountMenu;
-	JMenuItem quit,save,open,sendMoney,requestMoney;
-	Container cp;
+	private JMenuBar jMenuBar;
+	private JMenu fileMenu,accountMenu;
+	private JMenuItem quit,save,open,sendMoney,requestMoney;
+	
+	private JLabel initMessage; 
+	
+	private Container cp;
+	
+	
 
 	public static void main(String[] args)  {
 		
@@ -26,7 +32,7 @@ public class PayGateApp extends JFrame implements ActionListener {
 		
 	}
 	
-	public PayGateApp() {
+	public PayGateApp() {		
 		super("PayGate");
 		setSize(450,550);
 		setResizable(false);
@@ -44,13 +50,72 @@ public class PayGateApp extends JFrame implements ActionListener {
 		jMenuBar.add(fileMenu);
 		jMenuBar.add(accountMenu);
 		
+		initScreen();
+		
+		
+		cp.add(initMessage);
+		
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
+	
+	private void initScreen() {
+		
+		//if account < 0 then display
+		
+		initMessage = new JLabel("Your account is empty" +
+								"Deposit money or " + 
+								"request Money");
+		
+		
+		// else display account summary
+		
+	}
+
+	private void open() {
+		
+	}
+	
+	private void save() {
+		
+	}
+	
+	private void depositMoney() {
+		
+	}	
+	
+	private void requestMoney() {
+		
+	}	
+	
+	private void sendMoney() {
+		
+	}
+	
+
+	
+
+	
 
 	public void actionPerformed(ActionEvent e) {
 		
+		if(e.getActionCommand().equals("Quit")) {
+			JOptionPane.showMessageDialog(null, "System exits");
+			System.exit(0);
+		} else if(e.getActionCommand().equals("Open")) {
+			
+		} else if(e.getActionCommand().equals("Save")) {
+			
+		} else if(e.getActionCommand().equals("Send Money")) {
+			
+		} else if(e.getActionCommand().equals("Request Money")) {
+			
+		} else if(e.getActionCommand().equals("Send Money")) {
+			
+		} else {
+			JOptionPane.showMessageDialog(null, "start by selecting menu");
+		}
 		
 	}
 	
@@ -58,8 +123,11 @@ public class PayGateApp extends JFrame implements ActionListener {
 		fileMenu = new JMenu("File");
 		
 		quit = new JMenuItem("Quit");
+		quit.addActionListener(this);
 		open = new JMenuItem("Open");
+		open.addActionListener(this);
 		save = new JMenuItem("save");
+		save.addActionListener(this);
 		
 		fileMenu.add(open);
 		fileMenu.add(save);
@@ -70,7 +138,9 @@ public class PayGateApp extends JFrame implements ActionListener {
 		accountMenu = new JMenu("Account");
 		
 		sendMoney = new JMenuItem("Send Money");
-		requestMoney = new JMenuItem("Send Money");
+		sendMoney.addActionListener(this);
+		requestMoney = new JMenuItem("Request Money");
+		requestMoney.addActionListener(this);
 		
 		accountMenu.add(sendMoney);
 		accountMenu.add(requestMoney);		
